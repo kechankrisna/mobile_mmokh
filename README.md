b2615b5f14ebfd9e2db0ccede3d9afdee0beea03
kechankrisna <ke.chankrisna168@gmail.com>
2023-08-24 15:18:09 +0700

    first init

---
+++ README.md
@@ -0,0 +1,2 @@
+# mobile_mmokh
+used in order to work with android service in native call and use flutter framework to build the layout ui/ux
---
+++ android/app/src/main/kotlin/com/example/mobile_mmokh/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mobile_mmokh.android.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
